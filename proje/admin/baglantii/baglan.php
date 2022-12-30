e18e00bc77cf49fd973c16b931761dfd7c26b8f3
Umut Tokoğlu <umuttokoglu@gmail.com>
2022-12-31 01:26:01 +0300

    Update baglan.php

--- proje/admin/baglantii/baglan.php
+++ proje/admin/baglantii/baglan.php
@@ -1,7 +1,7 @@
 <?php 
 
 try{
-	$db=new PDO("mysql:host=localhost;dbname=u303600910_proje;charset=utf8", 'u303600910_umut','gkKIcsh6QyG1');
+	$db=new PDO("mysql:host=localhost;dbname=u303600910_proje;charset=utf8", '','');
 	//echo"bağlantı başarılı";
 }
 
